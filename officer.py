1bdf31ade2e6959375322efa3c4c385581b5875f
TBard05 <191855809+TBard05@users.noreply.github.com>
2025-05-20 09:27:29 +0200

    officer.py

--- officer.py
+++ officer.py
@@ -1,6 +1,9 @@
+# Denna funktion skola vi ej taga i bruk, varför hon avkommenteras och förbliver intet
+
+
 # can approve accounts
 # can approve account credit
 
-class Officer:
-    pass
+#class Officer:
+    #pass
 
